792de60d085353baf459920a7551e02cebf23e29
Allan Farca <allan.farca@gmail.com>
2020-03-31 03:57:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word (string)
-  string = "Hello there, and how are you?".split (" ") 
-  new_string = []
+  new_string = string.split (" ")
   string.collect do |word|
     new_string << word.reverse
   end
